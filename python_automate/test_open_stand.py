01da89f469bc32d1e04f346aee8182cf3fd4258c
TATYANA TIMAKINA <infrablossom@gmail.com>
2021-09-01 13:34:25 +0300

    Task 4

--- python_automate/test_open_stand.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver import Chrome
-
-
-def test_open_stand():
-    with Chrome() as browser:
-        browser.get('https://qastand.valhalla.pw/')
-        print(f'{browser.current_url} is open')
