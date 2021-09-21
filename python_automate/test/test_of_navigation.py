71f9a6fb2071614ae190c4ceed8fd8fd18f06ab6
TATYANA TIMAKINA <infrablossom@gmail.com>
2021-09-21 20:35:39 +0300

    fix

--- python_automate/test/test_of_navigation.py
+++ python_automate/test/test_of_navigation.py
@@ -4,7 +4,7 @@ from functions import *
 def test_of_navigation():
     with Chrome() as browser:
         browser.get('https://qastand.valhalla.pw/profile')
-        wait = WebDriverWait(browser, 20)
+        wait = WebDriverWait(browser, 5)
         browser.maximize_window()
         login(browser)
         my_pet = browser.find_element_by_xpath('//*[@href="/my_pet"]')
