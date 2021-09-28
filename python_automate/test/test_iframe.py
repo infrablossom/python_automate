a7b829fde6f7139c4b9e414ccd3f58135302edea
TATYANA TIMAKINA <infrablossom@gmail.com>
2021-09-28 16:20:40 +0300

    'fix'

--- python_automate/test/test_iframe.py
+++ python_automate/test/test_iframe.py
@@ -8,4 +8,8 @@ def test_iframe():
         wait = WebDriverWait(browser, 5)
         wait.until(ec.frame_to_be_available_and_switch_to_it((By.ID, 'my_iframe')))
         wait_until_visible(browser, (By.ID, 'photo'))
+        confirm_btn = wait_until_clickable(browser, (By.XPATH, '//*[contains(text(), "Подтвердить")]'))
+        confirm_btn.click()
+        alert = WebDriverWait(browser, 5).until(ec.alert_is_present())
+        alert.accept()
         browser.switch_to.default_content()
--- python_automate/test/test_new_tab.py
+++ python_automate/test/test_new_tab.py
@@ -5,22 +5,17 @@ def test_new_tab():
     with Chrome() as browser:
         cookie_login(browser)
         browser.get('https://qastand.valhalla.pw/new_window_button')
-        tab = browser.window_handles
-        assert len(tab) == 1, f'Должна быть открыта 1 вкладка, но открылось: {len(tab)}'
+        tabs = browser.window_handles
+        assert len(tabs) == 1, f'Должна быть открыта 1 вкладка, но открылось: {len(tabs)}'
         new_tab_btn = wait_until_clickable(browser, (By.XPATH, '//*[contains(text(), "Открыть")]'))
         new_tab_btn.click()
-        tab_2 = browser.window_handles
-        browser.switch_to.window(tab_2[1])
-        assert len(tab_2) == 2, f'Должно быть открыто 2 вкладки, но открылось: {len(tab_2)}'
+        tabs = browser.window_handles
+        browser.switch_to.window(tabs[1])
+        assert len(tabs) == 2, f'Должно быть открыто 2 вкладки, но открылось: {len(tabs)}'
         send = wait_until_clickable(browser, (By.XPATH, '//*[contains(text(), "Отправить")]'))
         send.click()
         alert = WebDriverWait(browser, 5).until(ec.alert_is_present())
         alert.accept()
-        #добавила явное ожидание, когда будет 1 вкладка
-        #иногда тест падает, потому что считает вкладки до закрытия второй
         WebDriverWait(browser, 5).until(ec.number_of_windows_to_be(1))
-        tab_3 = browser.window_handles
-        assert len(tab_3) == 1, f'Должна быть открыта 1 вкладка, но открылось: {len(tab_3)}'
-
-
-
+        tabs = browser.window_handles
+        assert len(tabs) == 1, f'Должна быть открыта 1 вкладка, но открылось: {len(tabs)}'
